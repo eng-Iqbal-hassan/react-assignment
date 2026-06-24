d61820a762bf893bb20c0f0d087d6651e57e9e62
Iqbal Hassan <iqbal@192.168.1.11>
2026-06-24 12:53:49 +0500

    authentication flow is completed

--- src/routes/dashboard/header.tsx
+++ src/routes/dashboard/header.tsx
@@ -1,15 +1,25 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/supabase';
 import { TodoIcon } from '../../assets/svg';
-import { Button } from '../../components/primitives/Button';
+import { ProfileDropdown } from './profileDropdown.tsx';
 
 export function Header() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      setUser(data.user);
+    };
+
+    getUser();
+  }, []);
   return (
     <header className="w-full h-25 bg-green-800 py-6 px-37.5 flex items-center justify-between">
       <div className="bg-white text-green-800 h-full px-3 pb-5 pt-1.5 w-min custom-header-clip">
         <TodoIcon />
       </div>
-      <Button type="button" variant="plain" size="small" className="p-0">
-        Signout
-      </Button>
+      <ProfileDropdown user={user} />
     </header>
   );
 }
